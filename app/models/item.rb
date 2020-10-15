391ecdd0a97fafe657b754986ae90bce34ea7422
mai-horiguchi <mai.horiguchi@di-v.co.jp>
2020-10-15 18:50:09 +0900

    add ActiveHash

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
+
+  # ActiveStorageのアソシエーション
   has_one_attached :image
+
+  # Active_hashとのアソシエーション
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :sales_status
+  belongs_to_active_hash :shipping_fee_status
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :scheduled_delivery
+
 end
