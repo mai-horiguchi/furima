c7c45d519414bf7867b19928a21c85e009a0f6f1
mai-horiguchi <mai.horiguchi@di-v.co.jp>
2020-10-15 19:46:37 +0900

    update item_price.js

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,6 +1,8 @@
 window.addEventListener("DOMContentLoaded", () => {
+
   const path = location.pathname
   const pathRegex = /^(?=.*item)(?=.*edit)/
+
   if (path === "/items/new" || path === "/items" || pathRegex.test(path)) {
     //    出品ページの場合　||　出品ページの検証にかかった場合 || 商品編集の場合
     const priceInput = document.getElementById("item-price");
@@ -8,8 +10,10 @@ window.addEventListener("DOMContentLoaded", () => {
     const profitDom = document.getElementById("profit");
 
     priceInput.addEventListener("input", () => {
+
       const inputRegex = /^([1-9]\d*|0)$/
       const inputValue = document.getElementById("item-price").value;
+      
       if (inputRegex.test(inputValue)) {
       //.test:文字列を正規表現でチェックする     https://www.sejuku.net/blog/58332
         addTaxDom.innerHTML = Math.floor(inputValue * 0.1).toLocaleString();
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()  読み込みを高速化してくれる機能。
 require("@rails/activestorage").start()
 require("channels")
 require("../item_price")
